4f12a88d5992f3b708afe3747700ca01b5440fac
parvanaAkbari <parvanakemsanakbarii@gmail.com>
2025-05-05 11:19:27 +0500

    one directory is added

---
+++ project1/Main.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+class Student{
+     String name;
+     int id;
+     public family;
+}
+class family{
+    String motherName;
+    String fatherName;
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+}
+public class Main{
+    public static void main(String[] args) {
+        
+    }
+}
